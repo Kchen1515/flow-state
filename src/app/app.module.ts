a37b4e6383ab59048b409687e19bde14d0a96a67
Kchen1515 <kychen249@gmail.com>
2023-04-19 18:47:55 -0400

    skeleton nav/sidebar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
 
 @NgModule({
   declarations: [
@@ -11,8 +14,10 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule
+  ]
+})
+
+export class MaterialModule{}
